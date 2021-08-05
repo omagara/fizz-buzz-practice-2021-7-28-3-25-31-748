41b764b752411226a58b08cade554e96950a345d
omagara <omagara@oocl.com>
2021-08-05 13:52:01 +0800

    [FizzBuzz][Ramon]test:make test for multiple of 3 order number

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -5,15 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class FizzBuzzTest {
-//multiplesOf3
-// multiplesOf5
-//MultiplesOf7
+//multiplesOf3 fizz
+// multiplesOf5 buzz
+//MultiplesOf7 whizz
     @Test
     void should_return_order_number_when_count_off_given_normal_case_order_number(){
         int orderNumber = 1;
         FizzBuzz fizzbuzz = new FizzBuzz();
         String result = fizzbuzz.countOff(orderNumber);
         assertThat(result).isEqualTo("1");
+    }
+
+    @Test
+    void should_return_fizz_when_count_off_given_multiple_of_3(){
+        int orderNumber = 3;
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        String result = fizzbuzz.countOff(orderNumber);
+        assertThat(result).isEqualTo("Fizz");
     }
 
 
