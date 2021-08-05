6c96b816c99ec6a90d8ec9b84ac84df09cba45e1
omagara <omagara@oocl.com>
2021-08-05 13:46:53 +0800

    [FizzBuzz][Ramon]test:make test for normal case order number

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,25 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class FizzBuzzTest {
+//multiplesOf3
+// multiplesOf5
+//MultiplesOf7
+    @Test
+    void should_return_order_number_when_count_off_given_normal_case_order_number(){
+        int orderNumber = 1;
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        String result = fizzbuzz.countOff(orderNumber);
+        assertThat(result).isEqualTo("1");
+    }
+
+    
+
+
+
 }
+
+
